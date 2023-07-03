c50a423d47307a571672a25c2457ea1e8b74ea2f
joshmysore <joshmysore@college.harvard.edu>
2023-07-03 18:02:13 -0400

    more codes

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 
+# The dictionary holding the codes as keys and the corresponding column names as values
+codes_and_columns = {
+    '51110-10': 'RE - Cleaning: Payroll',
+    '51110-15': 'RE - Cleaning: Contract Service',
+    '51110-40': 'RE - Cleaning: Repairs & Maintenance',
+    '51110-50': 'RE - Cleaning-Supplies & Materials',
+    '61110-50': 'NRE - Cleaning-Supplies & Materials'  # Add more codes and column names as needed
+}
+
 # Step 1: Read the spreadsheet into a DataFrame
 df = pd.read_excel('12-month-statement.xlsx')
 
@@ -9,24 +18,24 @@ NRE_data = []
 RE_data = []
 
 # Initialize target_column to None
-target_column = None
+code_column = None
 
-# Find the column that contains "51110-50" or "61110-50"
+# Find the column that contains codes based on revenue code
 for col in df.columns:
-    if '51110-50' in df[col].values or '61110-50' in df[col].values:
-        target_column = col
+    if '40100-00' in df[col].values:
+        code_column = col
         break
 
 # Check if target_column is still None (i.e., the codes were not found)
-if target_column is None:
-    print("Could not find a column with '51110-50' or '61110-50'.")
+if code_column is None:
+    print("Could not find revenue column.")
 else:
     # Find the rows with the codes "51110-50" and "61110-50"
-    NRE_row = df[df[target_column] == '51110-50']
-    RE_row = df[df[target_column] == '61110-50']
+    RE_clean_supplies = df[df[code_column] == '51110-50']
+    NRE_clean_supplies = df[df[code_column] == '61110-50']
 
     # Check the number of rows found
-    if NRE_row.empty or RE_row.empty:
+    if NRE_clean_supplies.empty or RE_clean_supplies.empty:
         print("Could not find the rows with '51110-50' and/or '61110-50'.")
     else:
         # Iterate over each column
@@ -43,8 +52,8 @@ else:
                         dates.append(cell)
 
                         # Store the data in the corresponding rows
-                        NRE_data.append(NRE_row.iat[0, df.columns.get_loc(col)])
-                        RE_data.append(RE_row.iat[0, df.columns.get_loc(col)])
+                        NRE_data.append(NRE_clean_supplies.iat[0, df.columns.get_loc(col)])
+                        RE_data.append(RE_clean_supplies.iat[0, df.columns.get_loc(col)])
                         break
                 except ValueError:
                     # If the cell couldn't be converted to a date, continue to the next cell
